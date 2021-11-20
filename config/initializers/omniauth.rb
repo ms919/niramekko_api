84e51c9152e209d44dc93ad08d0db634e7c9147d
ms919 <sakuraimiki919@gmail.com>
2021-11-20 19:38:53 +0900

    Fix: session設定修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.allowed_request_methods = [:post, :get]
-  OmniAuth.config.silence_get_warning = true
+  # OmniAuth.config.silence_get_warning = true
   OmniAuth.config.path_prefix = '/api/v1/auth'
   # 認証キャンセル用処理
   OmniAuth.config.on_failure = Proc.new { |env|
@@ -14,4 +14,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
       }
     }
   provider :google_oauth2, Rails.application.credentials.dig(:omniauth, :google, :client_id), Rails.application.credentials.dig(:omniauth, :google, :client_secret)
+  provider :tiktok, Rails.application.credentials.dig(:omniauth, :tiktok, :app_id), Rails.application.credentials.dig(:omniauth, :tiktok, :secret)
 end
