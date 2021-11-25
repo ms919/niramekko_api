fdd94c7247496d8f20161b01568ebcbdf8d161b2
ms919 <sakuraimiki919@gmail.com>
2021-11-25 13:24:25 +0900

    Add: corsにcredentials設定追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.application.credentials.production[:domain] || 'localhost:8080' || 'lvh.me:8080'
+    origins Rails.application.credentials.production[:domain], 'localhost:8080', 'lvh.me:8080'
     resource '*',
       headers: :any,
       expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
