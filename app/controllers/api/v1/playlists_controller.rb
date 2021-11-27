c985d153ec39f95aa5253123397cb21a48ae645f
ms919 <sakuraimiki919@gmail.com>
2021-11-27 16:20:27 +0900

    Add: 各モード別プレイリスト抽出処理分岐 #3

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -2,7 +2,14 @@ module Api
   module V1
     class PlaylistsController < ApplicationController
       def index
-        videos = Video.order("RANDOM()").limit(5)
+        case params[:mode].to_i
+        when GameResult.modes[:normal] then
+          videos = Video.order("RANDOM()").limit(5)
+        when GameResult.modes[:dojyo] then
+          videos = Video.where(latest_top_flg: true).limit(5)
+        when GameResult.modes[:revenge] then
+          videos = Video.revenge_playlists(current_user.id).limit(5)
+        end
         render json: videos
       end
     end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,6 @@
 class Video < ApplicationRecord
   belongs_to :user
   has_many :laughed_videos, dependent: :destroy
+
+  scope :revenge_playlists, -> (user_id) {joins(:laughed_videos).where(laughed_videos: {user_id: user_id})}
 end
