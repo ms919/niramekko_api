82033107226deda5a46d790e2d46a7b7b0a5ea2f
ms919 <sakuraimiki919@gmail.com>
2021-12-03 16:36:28 +0900

    Add: user情報取得処理(GameResult) #5)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,15 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      before_action :logged_in?
 
       def show
-        user = User.find(current_user.id)
-        session[:revenge_flg] = Video.revenge_playlists(current_user.id).length >= 3 unless session[:revenge_flg]
-        render json: { user: user, revenge_flg: session[:revenge_flg] }
+        render status: :unauthorized unless logged_in?
+        user_id = current_user.id
+        user = User.select(:name, :image_url).find(user_id)
+        game_results = GameResult.where(user_id: user_id).group(:title_id).count
+        total_score = GameResult.where(user_id: user_id).sum(:score)
+        session[:revenge_flg] = Video.revenge_playlists(user_id).length >= 3 unless session[:revenge_flg]
+        render json: { user: user, total_score: total_score, game_results: game_results, revenge_flg: session[:revenge_flg] }
       end
     end
   end
--- app/models/game_result.rb
+++ app/models/game_result.rb
@@ -1,4 +1,7 @@
 class GameResult < ApplicationRecord
+  include TITLES
+
   belongs_to :user, optional: true
   enum mode: { normal: 0, dojyo: 1, revenge: 2 }
+  enum title_id: { gold: GOLD, iron: IRON, soil: SOIL, smile: SMILE, laugh: LAUGH}
 end
