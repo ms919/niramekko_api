bca5d44a397d993912acaa39c1fc3dd40cece47b
ms919 <sakuraimiki919@gmail.com>
2021-12-04 18:12:31 +0900

    Add: 認証処理実装 (トークンカラム追加) #1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_115735) do
+ActiveRecord::Schema.define(version: 2021_12_04_073553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 2021_12_03_115735) do
     t.integer "role", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
+    t.index ["token"], name: "index_users_on_token"
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
