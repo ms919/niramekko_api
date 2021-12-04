323a2a0ffb8ce7d0986308dda1da1b3f3a538fa2
ms919 <sakuraimiki919@gmail.com>
2021-12-04 15:10:57 +0900

    Add: hidden videos追加(playlists作成処理にhiddenvideoのフィルタリング追加) #8

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -2,13 +2,14 @@ module Api
   module V1
     class PlaylistsController < ApplicationController
       def index
+        user_id = logged_in? ? current_user.id : nil
         case params[:mode].to_i
         when GameResult.modes[:normal] then
-          videos = Video.order("RANDOM()").limit(5)
+          videos = Video.filter_hidden_videos(user_id).order("RANDOM()").limit(5)
         when GameResult.modes[:dojyo] then
-          videos = Video.where(latest_top_flg: true).order("RANDOM()").limit(5)
+          videos = Video.filter_hidden_videos(user_id).where(latest_top_flg: true).order("RANDOM()").limit(5)
         when GameResult.modes[:revenge] then
-          videos = Video.revenge_playlists(current_user.id).order("RANDOM()").limit(5)
+          videos = Video.filter_hidden_videos(user_id).revenge_playlists(user_id).order("RANDOM()").limit(5)
         end
         render json: videos
       end
--- app/models/video.rb
+++ app/models/video.rb
@@ -3,5 +3,6 @@ class Video < ApplicationRecord
   has_many :laughed_videos, dependent: :destroy
   has_many :hidden_videos, dependent: :destroy
 
+  scope :filter_hidden_videos, -> (user_id){ where.not(id: HiddenVideo.select(:video_id).where(user_id: user_id)) }
   scope :revenge_playlists, -> (user_id){ joins(:laughed_videos).where(laughed_videos: {user_id: user_id}).group(:id) }
 end
