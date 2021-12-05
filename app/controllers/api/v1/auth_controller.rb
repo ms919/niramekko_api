ff00bd82d96a86b4ca38451e2d160a7e2978fe32
ms919 <sakuraimiki919@gmail.com>
2021-12-06 00:00:17 +0900

    Fix: 認証時パス修正

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,12 +5,12 @@ module Api
         user = User.find_or_create_from_auth(request.env['omniauth.auth'])
         user.update!(token: SecureRandom.urlsafe_base64)
         # redirect_to "http://localhost:8080/callback?token=#{user.token}"
-        redirect_to Rails.application.credentials.production[:domain] << "/callback?token=#{user.token}"
+        redirect_to "https://" << Rails.application.credentials.production[:domain] << "/callback?token=#{user.token}"
       end
 
       def failure
         # redirect_to "http://lvh.me:8080/login"
-        redirect_to Rails.application.credentials.production[:domain] << "/login"
+        redirect_to "https://" << Rails.application.credentials.production[:domain] << "/login"
       end
     end
   end
