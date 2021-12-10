3f3dcd8135f0172b082f44f25a5dd46a65fc632d
ms919 <sakuraimiki919@gmail.com>
2021-12-10 21:29:00 +0900

    Add: video一覧・削除処理 #13

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -3,13 +3,28 @@ module Api
     class VideosController < ApplicationController
       before_action :filter_unauthenticated
 
+      def index
+        videos = Video.select(:id, :user_id, :latest_top_flg, :video_user, :data_video_id).where(user_id: current_user.id).order(:id).page(params[:page]).per(5)
+        render json: { videos: videos, total_pages: videos.total_pages }
+      end
+
       def new; end
+
       def create
         video = current_user.videos.build(video_params)
         if video.save
           render json: video, status: :created
         else
           render status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        begin
+          Video.destroy(params[:id])
+          render status: :accepted
+        rescue
+          render status: :bad_request
         end
       end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resource :videos, only: [:new, :create]
+      resources :videos, only: [:index, :new, :create, :destroy]
       resources :playlists, only: [:index]
       resources :game_results, only: [:create]
       resources :laughed_videos, only: [:create]
