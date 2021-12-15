ac794c3a38c95cd026618fa4742ba6e17ee9cb8c
ms919 <sakuraimiki919@gmail.com>
2021-12-15 23:12:57 +0900

    Add: tiktok用name分岐追加 #12

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   def self.find_or_create_from_auth(auth)
     provider = auth[:provider]
     uid = auth[:uid]
-    name = auth[:info][:name]
+    name = auth[:info][:name] ? auth[:info][:name] : auth[:info][:display_name]
     image_url = auth[:info][:image]
 
     self.find_or_create_by(provider: provider, uid: uid) do |user|
