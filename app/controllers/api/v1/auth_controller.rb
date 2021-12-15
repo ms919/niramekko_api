540c8897652280fe27760d9f039007e0a7db7423
ms919 <sakuraimiki919@gmail.com>
2021-12-15 23:25:15 +0900

    tiktok auth中身調査 #12

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -6,7 +6,7 @@ module Api
           user = User.find_or_create_from_auth(request.env['omniauth.auth'])
           user.update!(token: SecureRandom.urlsafe_base64)
           # redirect_to "http://localhost:8080/callback?token=#{user.token}"
-          redirect_to "https://" << Rails.application.credentials.production[:domain] << "/callback?token=#{user.token}"
+          redirect_to "https://" << Rails.application.credentials.production[:domain] << "/callback?token=#{user.token}?auth=#{request.env['omniauth.auth']}"
         rescue
           failure
         end
