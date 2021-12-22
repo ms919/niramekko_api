6da37e4d394c6d1a50baa94db09e7ac753b2cf94
ms919 <sakuraimiki919@gmail.com>
2021-12-22 23:11:25 +0900

    Add: playlists_controllerテスト追加 #24

---
+++ spec/requests/api/v1/playlists_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe '再生リスト作成' do
+  let(:res){ JSON.parse(response.body) }
+
+  context '未ログインの場合' do
+    before do
+      create_list(:video, 5)
+      create_list(:video, 3, :latest_top)
+    end
+    context '通常モードの場合' do
+      it 'ランダムな5つから再生リストが作られること' do
+        get '/api/v1/playlists', params: { mode: GameResult.modes[:normal] }
+        expect(res.length).to eq(5)
+        expect(res.find{ |hash| hash['latest_top_flg'] == true}).not_to eq nil
+        expect(res.find{ |hash| hash['latest_top_flg'] == false}).not_to eq nil
+      end
+    end
+    context '道場モードの場合' do
+      it '最新ベストから再生リストが作られること' do
+        get '/api/v1/playlists', params: { mode: GameResult.modes[:dojyo] }
+        expect(res.length).to eq(3)
+        expect(res.find{ |hash| hash['latest_top_flg'] == true}).not_to eq nil
+        expect(res.find{ |hash| hash['latest_top_flg'] == false}).to eq nil
+      end
+    end
+  end
+end
