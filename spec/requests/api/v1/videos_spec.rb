d69a2d6e0ea6448132b597feb593e084de7e5fbb
ms919 <sakuraimiki919@gmail.com>
2022-01-15 17:31:48 +0900

    Add: videos_controller.rb#update テスト追加 #26

--- spec/requests/api/v1/videos_spec.rb
+++ spec/requests/api/v1/videos_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 describe 'videos' do
   let(:video){ create(:video, user: user) }
   let(:user){ create(:user) }
-  before do
-    login_as(video.user)
-  end
 
   describe '#index' do
     let(:res){ JSON.parse(response.body) }
     it 'videos一覧が取得できること' do
+      login_as(video.user)
       get '/api/v1/videos', params: { page: 1 }
       expect(res['videos'].length).to eq(1)
       expect(res['total_pages']).to eq(1)
@@ -18,6 +16,7 @@ describe 'videos' do
 
   describe '#new' do
     it 'ログインチェックに引っかからないこと' do
+      login_as(video.user)
       get '/api/v1/videos/new'
       expect(response.status).to eq(204)
     end
@@ -25,14 +24,24 @@ describe 'videos' do
 
   describe '#create' do
     it 'videoがDBに登録されること' do
+      login_as(video.user)
       valid_params = { video_user: 'test_video_user', data_video_id: format("%019<number>d", number: 999) }
       expect { post '/api/v1/videos', params: valid_params, as: :json }.to change(Video, :count).by(+1)
       expect(response.status).to eq(201)
     end
   end
 
+  describe '#update' do
+    it '指定したビデオのcannot_play_flgがtrueになること' do
+      patch "/api/v1/videos/#{video.id}"
+      expect(Video.find(video.id).cannot_play_flg).to eq(true)
+      expect(response.status).to eq(202)
+    end
+  end
+
   describe '#destroy' do
     it '指定したビデオがvideoから削除されること' do
+      login_as(video.user)
       expect { delete "/api/v1/videos/#{video.id}" }.to change(Video, :count).by(-1)
       expect(response.status).to eq(202)
     end
