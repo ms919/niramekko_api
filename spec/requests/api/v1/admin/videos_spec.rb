53b06842de6ef741958072212ad7f5136931e57b
ms919 <sakuraimiki919@gmail.com>
2022-01-16 20:12:50 +0900

    Add: admin/videos_controller.rb#indexテスト追加 #28

--- spec/requests/api/v1/admin/videos_spec.rb
+++ spec/requests/api/v1/admin/videos_spec.rb
@@ -2,16 +2,26 @@ require 'rails_helper'
 
 describe 'admin/videos' do
   before do
-    @video = create(:video)
+    create(:video)
+    @video = create(:video, :cannot_play)
     login_as(create(:user, :admin))
   end
 
   describe '#index' do
     let(:res){ JSON.parse(response.body) }
-    it 'ビデオ一覧が取得できること' do
-      get '/api/v1/admin/videos', params: { page: 1 }
-      expect(res['videos'].length).to eq(1)
-      expect(res['total_pages']).to eq(1)
+    context 'cannot_play_flg == false' do
+      it '再生可能なビデオ一覧が取得できること' do
+        get '/api/v1/admin/videos', params: { page: 1, cannot_play_flg: false }
+        expect(res['videos'].length).to eq(1)
+        expect(res['total_pages']).to eq(1)
+      end
+    end
+    context 'cannot_play_flg == true' do
+      it '再生不可能なビデオ一覧が取得できること' do
+        get '/api/v1/admin/videos', params: { page: 1, cannot_play_flg: true }
+        expect(res['videos'].length).to eq(1)
+        expect(res['total_pages']).to eq(1)
+      end
     end
   end
 
