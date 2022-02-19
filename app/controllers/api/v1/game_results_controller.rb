2f0f80c824933f13f9cd33ec565257214c8cf933
ms919 <sakuraimiki919@gmail.com>
2022-02-19 14:41:11 +0900

    Fix: controllerリファクタリング漏れ #29

--- app/controllers/api/v1/game_results_controller.rb
+++ app/controllers/api/v1/game_results_controller.rb
@@ -2,13 +2,7 @@ module Api
   module V1
     class GameResultsController < ApplicationController
       def create
-        # if logged_in?
-        #   game_result = current_user.game_results.build(game_results_params)
-        # else
-        #   game_result = GameResult.new(game_results_params)
-        # end
         game_result = logged_in? ? current_user.game_results.build(game_results_params) : GameResult.new(game_results_params)
-
         if game_result.save
           render json: game_result.title
         else
